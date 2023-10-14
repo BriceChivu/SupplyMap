e454add34abc256eb99389c998ffe77b759ca398
Brice <bricechivu@gmail.com>
2023-10-14 19:37:32 +0800

    improved

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# This is the app.py file.
+
 import streamlit as st
 from modules.data_processing import process_data
 from modules.streamlit_logger import StreamlitMemoryHandler
@@ -5,16 +7,29 @@ import logging
 
 # Set up custom logging handler
 logger = logging.getLogger()
-handler = StreamlitMemoryHandler(st.session_state)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+# Check if StreamlitMemoryHandler is already in logger's handlers
+if not any(isinstance(h, StreamlitMemoryHandler) for h in logger.handlers):
+    handler = StreamlitMemoryHandler(st.session_state)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+else:
+    # If the handler is already there, clear other handlers to avoid duplicates
+    logger.handlers = [
+        h for h in logger.handlers if isinstance(h, StreamlitMemoryHandler)
+    ]
 
 
 # Main App
 def main():
     st.title("Supply Chain Optimization App")
+
+    # Store uploaded and processed dataset in session state
+    if "processed_df" not in st.session_state:
+        st.session_state.processed_df = None
 
     # Tab-like sections using st.radio
     tab = st.radio("Go to", ["Upload Dataset", "View Logs"])
@@ -29,21 +44,37 @@ def main():
 
             try:
                 df = pd.read_csv(uploaded_file)
-                processed_df = process_data(df)
-                st.write(
-                    processed_df.head()
-                )  # Display the first few rows of the processed dataframe
+                st.session_state.processed_df = process_data(df, uploaded_file.name)
+                if st.session_state.processed_df is not None:
+                    st.write(st.session_state.processed_df.head())
+                else:
+                    st.error(
+                        "The uploaded dataset couldn't be processed correctly. Please check the logs for more details."
+                    )
             except Exception as e:
                 st.error(f"An error occurred: {e}")
                 logger.error(f"Failed to process the uploaded file: {e}")
+        elif st.session_state.processed_df is not None:
+            st.write(st.session_state.processed_df.head())
 
     elif tab == "View Logs":
         # Display Logs
         st.header("Logs")
         # Display logs from session state
         if "logs" in st.session_state:
-            for log in st.session_state.logs:
-                st.text(log)
+            # Concatenate logs and display in a text area
+            st.text_area(
+                "Logs", "\n".join(st.session_state.logs), height=800, max_chars=None
+            )
+
+            # Provide a button to download the logs as a .log file
+            log_content = "\n".join(st.session_state.logs)
+            st.download_button(
+                label="Download Logs",
+                data=log_content,
+                file_name="logs.log",
+                mime="text/plain",
+            )
 
 
 if __name__ == "__main__":
